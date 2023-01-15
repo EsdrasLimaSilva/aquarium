52ddecac8117ea2364c2c42ead165ca0c5666bee
Esdras Silva <esdraslimasilva83@gmail.com>
2023-01-15 13:39:57 -0300

    Fix build bug

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  images: {
-    domains: ["lh3.googleusercontent.com", "cdn.sanity.io"],
-  },
+    reactStrictMode: false,
+    swcMinify: false,
+    images: {
+        domains: ["lh3.googleusercontent.com", "cdn.sanity.io"],
+    },
 };
 
 module.exports = nextConfig;
